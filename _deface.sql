582337ba706cd6ea69b0d297b13ba91fe4463009
Tiwy57 <postmaster@lyokoweb.fr>
2013-06-18 15:49:52 +0200

    Ajout de la structure SQL

---
+++ _deface.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `_deface` (
+  `file_id` int(11) NOT NULL AUTO_INCREMENT,
+  `path` varchar(255) NOT NULL,
+  `hash` varchar(32) NOT NULL,
+  UNIQUE KEY `file_id` (`file_id`),
+  UNIQUE KEY `path` (`path`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4051 ;
